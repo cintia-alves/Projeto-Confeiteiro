9cd695c238802a196c95d139d38fb31aac12b090
maria-tatiane <mariatatiane@alu.ufc.br>
2025-06-25 22:16:54 -0300

    Area do Cardapio
    Area do Cardapio com o titulo "cardapio" centralizado

---
+++ src/Componentes/Cardapio/AreaCardapio/AreaCardapio.jsx
@@ -0,0 +1,9 @@
+import './AreaCardapio.css';
+
+export default function AreaCardapio() {
+  return (
+    <div className="area-cardapio">
+      <h2 className="titulo-cardapio">Cardápio</h2>
+    </div>
+  );
+}
--- src/Componentes/Cardapio/Cardapio.jsx
+++ src/Componentes/Cardapio/Cardapio.jsx
@@ -1,8 +1,19 @@
-import './Cardapio.css'
+// import './Cardapio.css'
+// export default function Cardapio() {
+// return (
+// <div className="cardapio">
+//     <p>Área do Cardápio (em branco por enquanto)</p>
+// </div>
+// );
+// }
+
+import './Cardapio.css';
+import AreaCardapio from './AreaCardapio/AreaCardapio';
+
 export default function Cardapio() {
-return (
-<div className="cardapio">
-    <p>Área do Cardápio (em branco por enquanto)</p>
-</div>
-);
+  return (
+    <div className="cardapio">
+      <AreaCardapio />
+    </div>
+  );
 }
