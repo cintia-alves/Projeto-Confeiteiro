b940fdc870fd9250a51523c0afeb10d970673018
cintia-alves <130922834+cintia-alves@users.noreply.github.com>
2025-06-27 00:01:42 -0300

    Tentando fazer nome, descriçao e informaçoes editaveis

--- src/Componentes/perfil/DescricaoLoja/DescricaoLoja.jsx
+++ src/Componentes/perfil/DescricaoLoja/DescricaoLoja.jsx
@@ -1,11 +1,23 @@
 import './DescricaoLoja.css';
+import { useState } from 'react';
 
-export default function DescricaoLoja() {
-return (
-<div className="descricao-loja">
-<p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra, nisl sed
-</p>
-</div>
-);
+export default function DescricaoLoja({ isEditando }) {
+  const [descricao, setDescricao] = useState(
+    'Escreva aqui a descrição da sua loja.\nPara que seus clientes conheçam melhor o seu trabalho e os produtos que você vende.'
+  );
+
+  return (
+    <div className="descricao-loja">
+      {isEditando ? (
+        <textarea
+          className="descricao-textarea"
+          value={descricao}
+          onChange={(e) => setDescricao(e.target.value)}
+          maxLength={240}
+        />
+      ) : (
+        <p>{descricao}</p>
+      )}
+    </div>
+  );
 }
--- src/Componentes/perfil/InformacoesLoja/InformacoesLoja.jsx
+++ src/Componentes/perfil/InformacoesLoja/InformacoesLoja.jsx
@@ -1,21 +1,53 @@
 import './InformacoesLoja.css';
 import { LuMapPin, LuCreditCard, LuClock3 } from 'react-icons/lu';
+import { useState } from 'react';
 
-export default function InformacoesLoja() {
-return (
-<div className="informacoes-loja">
-<div className="info-linha">
-<LuMapPin className="icone" />
-<span>Endereço</span>
-</div>
-<div className="info-linha">
-<LuCreditCard className="icone" />
-<span>Formas de Pagamento</span>
-</div>
-<div className="info-linha">
-<LuClock3 className="icone" />
-<span>Horário de Funcionamento</span>
-</div>
-</div>
-);
+export default function InformacoesLoja({ isEditando }) {
+  const [endereco, setEndereco] = useState('Endereço');
+  const [pagamento, setPagamento] = useState('Formas de Pagamento');
+  const [horario, setHorario] = useState('Horário de Funcionamento');
+
+  return (
+    <div className="informacoes-loja">
+      <div className="info-linha">
+        <LuMapPin className="icone" />
+        {isEditando ? (
+          <input
+            type="text"
+            className="info-input"
+            value={endereco}
+            onChange={(e) => setEndereco(e.target.value)}
+          />
+        ) : (
+          <span>{endereco}</span>
+        )}
+      </div>
+      <div className="info-linha">
+        <LuCreditCard className="icone" />
+        {isEditando ? (
+          <input
+            type="text"
+            className="info-input"
+            value={pagamento}
+            onChange={(e) => setPagamento(e.target.value)}
+          />
+        ) : (
+          <span>{pagamento}</span>
+        )}
+      </div>
+      <div className="info-linha">
+        <LuClock3 className="icone" />
+        {isEditando ? (
+          <input
+            type="text"
+            className="info-input"
+            value={horario}
+            onChange={(e) => setHorario(e.target.value)}
+          />
+        ) : (
+          <span>{horario}</span>
+        )}
+      </div>
+    </div>
+  );
 }
