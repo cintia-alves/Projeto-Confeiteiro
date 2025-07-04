b12441c0451c29a741995624c19975c633146ef0
maria-tatiane <mariatatiane@alu.ufc.br>
2025-07-04 12:36:20 -0300

    AreaDosCardapio
    atualizei a area das categorias inclusa no cardapio

--- src/Componentes/Cardapio/AreaCardapio/AreaCardapio.jsx
+++ src/Componentes/Cardapio/AreaCardapio/AreaCardapio.jsx
@@ -1,9 +1,11 @@
 import './AreaCardapio.css';
+import AreaDasCategorias from '../AreaDasCategorias/AreaDasCategorias';
 
 export default function AreaCardapio() {
   return (
     <div className="area-cardapio">
       <h2 className="titulo-cardapio">Cardápio</h2>
+      <AreaDasCategorias />
     </div>
   );
 }
---
+++ src/Componentes/Cardapio/AreaDasCategorias/AreaDasCategorias.jsx
@@ -0,0 +1,9 @@
+import './AreaDasCategorias.css';
+
+export default function AreaDasCategorias() {
+  return (
+    <div className="area-categorias">
+      <h3 className="titulo-categorias">Categorias</h3>
+    </div>
+  );
+}
