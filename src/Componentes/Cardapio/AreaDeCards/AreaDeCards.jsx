48b2c688d70830b41a640784556cbe6093f704f6
Viviane791 <araujoviviane@alu.ufc.br>
2025-07-06 17:48:27 -0300

    Botão de remover card funcional

--- src/Componentes/Cardapio/AreaDeCards/AreaDeCards.jsx
+++ src/Componentes/Cardapio/AreaDeCards/AreaDeCards.jsx
@@ -1,11 +1,16 @@
 import './AreaDeCards.css';
 import CardProduto from '../CardProduto/CardProduto';
 
-export default function AreaDeCards({ produtos, onEditar }) {
+export default function AreaDeCards({ produtos, onEditar, onRemover }) {
   return (
     <div className="area-cards">
       {produtos.map((doce) => (
-        <CardProduto key={doce.id} produto={doce} onEditar={onEditar} />
+        <CardProduto
+          key={doce.id}
+          produto={doce}
+          onEditar={onEditar}
+          onRemover={onRemover} 
+        />
       ))}
     </div>
   );
