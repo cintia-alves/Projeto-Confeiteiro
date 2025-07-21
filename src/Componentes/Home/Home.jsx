b05f5083806f5a1e4b6fe14084824037e8dc867c
maria-tatiane <mariatatiane@alu.ufc.br>
2025-07-21 09:34:04 -0300

    arrumei o css e tirei o botao de cadastro

--- src/Componentes/Home/Home.jsx
+++ src/Componentes/Home/Home.jsx
@@ -18,7 +18,7 @@ function Home () {
     <div id="div_all">
       <div id="div_header"> 
         <div id="div_header_logo" className='header'>
-          <h3 id="div_logo"> Logo </h3> 
+          <h3 id="div_logo"> Method </h3> 
         </div>
         <div id="div_header_botao" className='header'>
              <button id="botao_div_header" onClick={handleEntrar}>Entrar</button>
@@ -27,9 +27,9 @@ function Home () {
       </div>
       <div id="div_body">
         <div id="div_body_left" className='testediv'>
-        <h1> GeraCatálogos: Sua <br /> doceria na internet, do <br /> jeitinho que você<br /> precisa!</h1>
-        <p> Você ama preparar doces, bolos e delícias caseiras, mas sente<br /> dificuldade na hora de usar a internet para montar seu cardápio<br /> online? A gente entende, e criou a solução perfeita para você.</p>
-        <button id="botao_entrar_body_left"> Fazer cadastro / entrar</button>
+        <h1 id="texto_titulo"> Meu Doce Cardápio: <br />Sua doceria na internet,<br /> do  jeitinho que você<br /> precisa!</h1>
+        <p id="texto_home"> Você ama preparar doces, bolos e delícias caseiras, mas sente<br /> dificuldade na hora de usar a internet para montar seu cardápio<br /> online? A gente entende, e criou a solução perfeita para você.</p>
+       
 
         </div>
         <div id="div_body_right" className='testediv'>
